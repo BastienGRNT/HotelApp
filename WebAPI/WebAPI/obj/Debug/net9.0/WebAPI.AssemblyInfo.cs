a2f363353cc75f0a723a2bef2aacf750a5c0ec39
Bastien <bastien.grenot@viacesi.fr>
2024-12-02 16:25:18 +0100

    TEST

--- WebAPI/WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32657159d1dd039b94d41d675cc8dd3c1cb03d03")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
